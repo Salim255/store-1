1c1b7ccff9b7c6a6642bc0b00dd04a0c7a2dabc4
Salim <doshka23@gmail.com>
2025-07-16 14:13:58 +0200

    build: project stracture

---
+++ housori/src/app/navbar/navbar.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+import { NavbarRoutingModule } from "./navbar-routing.module";
+
+@NgModule({
+  imports:[NavbarRoutingModule]
+})
+export class NavbarModule { }
