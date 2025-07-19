aa3815fd59c81e6af3bdddf191e0fd28e4b77e1c
Salim <salim.hassan.welleat@gmail.com>
2025-07-19 14:57:45 +0200

    setup: products page stacture

--- housori/src/app/features/products/components/filter/filter.component.ts
+++ housori/src/app/features/products/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   selector: "app-filter",
@@ -9,5 +9,37 @@ import { Component } from "@angular/core";
 
 export class FilterComponent {
   products = ['Laptop', 'Phone', 'Headphones']; // Example
+
   selectedProduct = '';
+  customPopoverOptions = {
+    cssClass: 'custom-popover'
+  };
+
+  companyValue = signal<string>("all");
+  categoryValue = signal<string>("all");
+  sortValue = signal<string>("all");
+
+  categoryOptions = ['brown', 'blonde', 'Red'];
+  companyOptions = ['brown', 'blonde', 'Red'];
+  sortOptions = ['brown', 'blonde', 'Red'];
+  constructor(){}
+
+  onCategoryChange(event: any){
+    const value = event.detail.value;
+    if (value) this.categoryValue.set(value);
+    console.log(event.detail.value)
+  }
+
+  onCompanyChange(event: any){
+    const value = event.detail.value;
+    if (value) this.companyValue.set(value);
+    console.log(event.detail.value)
+  }
+
+  onSortChange(event: any){
+    const value = event.detail.value;
+    if (value) this.sortValue.set(value);
+    console.log(event.detail.value)
+  }
+
 }
