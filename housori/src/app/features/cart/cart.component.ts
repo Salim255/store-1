1bd53fb246ba8a0c6d599bfc12d861bc0179ddfa
Salim <doshka23@gmail.com>
2025-07-20 18:11:54 +0200

    built: cart layout

--- housori/src/app/features/cart/cart.component.ts
+++ housori/src/app/features/cart/cart.component.ts
@@ -8,5 +8,11 @@ import { Component } from "@angular/core";
 })
 
 export class CartComponent {
+   totalDetails = [
+    { name: 'subtotal', value: 45 },
+    {name: 'shipping', value: 45},
+    {name: 'tax', value: 45},
+    { name: 'order total', value: 45 },
+  ]
   constructor() {}
 }
