81c25e779f0ddd9a61e3673bd3f297f95c655cfa
Salim <doshka23@gmail.com>
2025-07-21 02:23:15 +0200

    built: single product layout

--- housori/src/app/features/products/products-routing.module.ts
+++ housori/src/app/features/products/products-routing.module.ts
@@ -5,8 +5,13 @@ import { ProductsComponent } from "./products.component";
 const routes: Routes =[
   {
     path: '',
-    component: ProductsComponent // Assuming ProductsComponent is defined elsewhere
-  }
+    component: ProductsComponent, // Assuming ProductsComponent is defined elsewhere
+
+  },
+     {
+        path: ':productId',
+        loadChildren:  () => import('../single-product/single-product.module').then( m => m.SingleProductModule)
+      }
 ]
 @NgModule({
   imports: [RouterModule.forChild(routes)],
