226b1bcca26067690f15bdc4830bfa72941e1ab5
Salim <doshka23@gmail.com>
2025-07-31 15:18:02 +0200

    built: JWT module

--- server/src/modules/users/services/users.service.ts
+++ server/src/modules/users/services/users.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { UserModel } from '../model/user.model';
 import { CreateUserDto } from '../dto/users.dto';
 import { User } from '../schema/user.schema';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
-  constructor(private userModel: UserModel) {}
+  logger = new Logger();
+  constructor(
+    private userModel: UserModel,
+    private jwtService: JwtService,
+  ) {}
 
   async signup(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = await this.userModel.create(createUserDto);
+    // Data sanitization
+    const { lastName, firstName, email, password, passwordConfirm } =
+      createUserDto;
+    const createdUser = await this.userModel.create({
+      lastName,
+      firstName,
+      email,
+      password,
+      passwordConfirm,
+    });
+
+    const newUser = createdUser;
+    // JWT
+    // 1) Build the payload, _id and the secret
+    const payload = { _id: newUser._id };
+    const token = await this.jwtService.signAsync(payload);
+    this.logger.log(token);
     return createdUser;
   }
 }
