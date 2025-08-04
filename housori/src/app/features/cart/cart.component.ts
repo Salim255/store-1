9047d2fadc74d4009d60ad150fc1826bdb4bceaf
Salim <doshka23@gmail.com>
2025-08-05 01:32:52 +0200

    built: display carts items

--- housori/src/app/features/cart/cart.component.ts
+++ housori/src/app/features/cart/cart.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { CartDetails, CartService } from "./services/cart-service";
+import { Subscription } from "rxjs";
+import { Product } from "../products/model/product.model";
 
 @Component({
   selector: 'app-cart',
@@ -8,11 +11,30 @@ import { Component } from "@angular/core";
 })
 
 export class CartComponent {
+  cartState = signal<CartDetails | null>(null);
    totalDetails = [
     { name: 'subtotal', value: 45 },
     {name: 'shipping', value: 45},
     {name: 'tax', value: 45},
     { name: 'order total', value: 45 },
   ]
-  constructor() {}
+
+  cartStateSubscription!: Subscription;
+  constructor(private cartService : CartService ) {}
+
+  ngOnInit(): void {
+    this.subscribeToCartState();
+  }
+
+  subscribeToCartState(){
+    this.cartStateSubscription =
+      this.cartService.getCartState.subscribe((cartState) => {this.cartState.set(cartState)});
+  }
+
+  get cartItems(): Product[]{
+    return this.cartState()?.cartItems ?? [];
+  }
+  ngOnDestroy(): void {
+    this.cartStateSubscription?.unsubscribe();
+  }
 }
