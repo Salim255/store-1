dc872869e20dbb6aa4f399f8e09e053fe1dcbafd
Salim <salim.hassan.welleat@gmail.com>
2025-08-05 12:38:44 +0200

    integrated the cartitems

--- housori/src/app/features/checkout/checkout.component.ts
+++ housori/src/app/features/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
-import {Component} from "@angular/core"
+import {Component, OnDestroy, OnInit, signal} from "@angular/core";
+import { CartService } from "../cart/services/cart-service";
+import { Subscription } from "rxjs";
+import { TotalOrderDetails } from "src/app/shared/components/order-total/order-total.component";
 
 @Component({
   selector: 'app-checkout',
@@ -7,12 +10,27 @@ import {Component} from "@angular/core"
   standalone: false,
 })
 
-export class CheckoutComponent{
-  totalDetails = [
-    { name: 'subtotal', value: 45 },
-    {name: 'shipping', value: 45},
-    {name: 'tax', value: 45},
-    { name: 'order total', value: 45 },
-  ]
-  constructor(){}
+export class CheckoutComponent implements OnInit, OnDestroy{
+  private cartStateSubscription!: Subscription;
+  totalDetails = signal< TotalOrderDetails | null>(null);
+
+  constructor (private cartService : CartService ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.subscribeToCartState();
+  }
+  subscribeToCartState(){
+    this.cartStateSubscription = this.cartService.getCartState.subscribe(cartState => {
+      if (cartState) {
+        const {cartItems, ...rest} = cartState;
+        this.totalDetails.set(rest);
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.cartStateSubscription?.unsubscribe();
+  }
 }
