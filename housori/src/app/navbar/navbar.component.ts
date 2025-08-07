ba7eec3a4f5e14dcd5a7f583b4fc49fe44f7969f
Salim <salim.hassan.welleat@gmail.com>
2025-08-07 02:03:57 +0200

    added: app form layout

--- housori/src/app/navbar/navbar.component.ts
+++ housori/src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit, signal } from "@angular/core";
 import { CartDetails, CartService } from "../features/cart/services/cart-service";
 import { Subscription } from "rxjs";
+import { AuthType } from "../features/auth/services/auth.service";
+import { AuthService } from "../features/auth/services/auth.service";
 
 @Component({
   selector: "app-navbar",
@@ -11,18 +13,30 @@ import { Subscription } from "rxjs";
 
 export class NavbarComponent implements OnInit, OnDestroy {
   cartState = signal< CartDetails | null>(null);
+  authType = signal<AuthType | null>(null)
   cartStateSubscription!: Subscription;
-  constructor(private cartService: CartService){}
+  authTypeSubscription!: Subscription;
+  constructor(
+    private authService: AuthService,
+    private cartService: CartService,
+  ){}
 
   ngOnInit(): void {
     this.subscribeToCartState();
-      document.body.style.overflow = 'hidden';
+    this.subscribeToAuthType();
+    if (this.authType() !== AuthType.GUEST) document.body.style.overflow = 'hidden';
+  }
+
+  subscribeToAuthType(){
+    this.authTypeSubscription = this.authService.getAuthType.subscribe(type => {
+      this.authType.set(type);
+      this.disableScroll();
+    })
   }
 
   subscribeToCartState(){
     this.cartStateSubscription = this.cartService.getCartState.subscribe(cartState => {
-      console.log(cartState, "Hello from here")
-      this.cartState.set(cartState)
+      this.cartState.set(cartState);
     })
   }
 
@@ -31,8 +45,23 @@ export class NavbarComponent implements OnInit, OnDestroy {
     return numItem ?? 0;
   }
 
-  onAlert(){
+  disableScroll():void{
+      if (this.authType() === AuthType.GUEST) {
+        document.body.style.overflow = 'auto';
+      } else {
+        document.body.style.overflow = 'hidden';
+      }
+    //else document.body.style.overflow = 'none';
+  }
+  onSignUp(): void{
+    this.authService.setAuthType(AuthType.LOGIN);
+  }
+  onRegister(): void{
+    this.authService.setAuthType(AuthType.SIGNUP);
+  }
 
+  get getAuthType(): boolean {
+    return this.authType() === AuthType.GUEST;
   }
   ngOnDestroy(): void {
     this.cartStateSubscription.unsubscribe();
