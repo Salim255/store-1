e245afb8de95581781a01c54afcf0dc7140f52a1
Salim <doshka23@gmail.com>
2025-08-09 17:39:53 +0200

    built: autologin

--- housori/src/app/features/auth/services/auth.service.ts
+++ housori/src/app/features/auth/services/auth.service.ts
@@ -14,14 +14,12 @@ export class AuthService {
   private userAuth = new BehaviorSubject<UserAuthentication | null>(null);
   private authTypeSubject = new BehaviorSubject<AuthType>(AuthType.GUEST);
 
-
   constructor(private authHttpService: AuthHttpService ){}
 
   setAuthData(authData: { _id : string, expireIn: string } ): void {
-    const expirationTime = new Date(new Date().getTime() + +authData.expireIn);
+    const expirationTime = new Date(new Date().getTime());
     const builder = new UserAuthentication( authData._id, expirationTime);
     this.userAuth.next(builder);
-
   }
 
   get userIsAuthenticated(): Observable<boolean> {
@@ -37,7 +35,32 @@ export class AuthService {
       map((data )=> data?.userId ?? null)
     )
   }
-  setAuthType(authType: AuthType ){
+
+  private autoLogout(): void {
+    this.logout();
+  }
+
+  async logout(): Promise<void>{
+    this.userAuth.next(null);
+  }
+
+  autoLogin(): Observable<boolean>{
+    return this.authHttpService.authStatus().pipe(
+      map((response) => {
+        const authStatus: boolean = response.data.authenticated;
+        const buildUserAuth = new UserAuthentication(response.data.id, new Date());
+
+        if (!response.data.authenticated) {
+          this.autoLogout();
+          return authStatus;
+        };
+        this.userAuth.next(buildUserAuth);
+        return authStatus;
+      })
+    )
+  }
+
+  setAuthType(authType: AuthType ): void{
     this.authTypeSubject.next(authType);
   }
 
