2a9b88a7f99d55ddd66f7243c1f64cb110f7369d
Salim <doshka23@gmail.com>
2025-08-18 17:43:50 +0200

    refactored: checkout template & css

--- housori/src/app/features/about/about.component.ts
+++ housori/src/app/features/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-about',
@@ -7,4 +8,11 @@ import { Component } from "@angular/core";
   standalone: false
 })
 
-export class AboutComponent {}
+export class AboutComponent {
+  aboutHero = "https://images.unsplash.com/photo-1693578616322-c8abe6c7393d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzR8fEhpZ2glMjBxdWFsaXR5JTIwc29mYXxlbnwwfHwwfHx8MA%3D%3D";
+
+  constructor(private router: Router) {}
+  onShopNow(): void{
+    this.router.navigate(['/products']);
+  }
+}
