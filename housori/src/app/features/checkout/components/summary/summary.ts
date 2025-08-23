4f201c0033ec773a083c96cf7539e88de21d3566
Salim <doshka23@gmail.com>
2025-08-23 02:02:34 +0200

    integrated: checkout orders summary

--- housori/src/app/features/checkout/components/summary/summary.ts
+++ housori/src/app/features/checkout/components/summary/summary.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
-import { CartService } from "src/app/features/cart/services/cart-service";
+import { Component, signal } from "@angular/core";
+import { Subscription } from "rxjs";
+import { CartDetails, CartItem, CartService } from "src/app/features/cart/services/cart-service";
 
 @Component({
   selector: 'app-summary',
@@ -9,11 +10,27 @@ import { CartService } from "src/app/features/cart/services/cart-service";
 })
 
 export class SummaryComponent {
- cartItems = [1,2];
-  constructor() {}
+  private cartSubscription!: Subscription;
+  cart = signal< CartDetails | null>(null);
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-
+    this.subscribeToCart();
   }
 
+  subscribeToCart():void{
+    this.cartSubscription = this.cartService.getCartState.subscribe(cart => {
+      console.log(cart);
+      this.cart.set(cart);
+    })
+  }
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.cartSubscription?.unsubscribe();
+  }
+
+  get getItems(): CartItem[] | []{
+    return this.cart()?.cartItems || []
+  }
 }
