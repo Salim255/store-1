5a5fe05fa416e0fd4d60643fa378220d2cfc72d5
Salim <doshka23@gmail.com>
2025-08-29 20:39:07 +0200

    added: variables to angular env

--- housori/src/environments/environment.prod.ts
+++ housori/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://127.0.0.1:3000/api/v1',
+  stripePK: 'pk_test_51RuiiOFrWj4JpsgpN2qiw1eF9be81IdGluFnvD3yq2nWpjCCEVHTeLsMZvXbA29F8RBejx7gl4HZGLcOdEkKIbnk00c9kXGmYY'
 };
