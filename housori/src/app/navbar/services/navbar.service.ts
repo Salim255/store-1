94a6ae2e56c19aa9b5534f6ace92daa7bba30a1f
Salim <salim.hassan.welleat@gmail.com>
2025-09-07 23:23:05 +0200

    all is good

--- housori/src/app/navbar/services/navbar.service.ts
+++ housori/src/app/navbar/services/navbar.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
 
-@Injectable({providedIn: 'root'})
+export type LikeContent = {
+  path: string;
+  label: string;
+   exact?: boolean;
+}
 
+@Injectable({providedIn: 'root'})
 export class NavbarService {
-  private closeSidebarSubject = new BehaviorSubject<'close' | null>(null)
+  private closeSidebarSubject = new BehaviorSubject<'close' | null>(null);
+  navLinks:  LikeContent[] = [
+  { path: '/home', label: 'Home', exact: true },
+  { path: '/about', label: 'About' },
+  { path: '/products', label: 'Products' },
+  { path: '/orders', label: 'Orders' },
+  { path: '/checkout', label: 'Checkout' },
+  { path: '/cart', label: 'Cart' }
+];
   constructor(){}
 
   onCloseSideBar(): void{
