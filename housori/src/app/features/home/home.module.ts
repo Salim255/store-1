be7f5171e212dbb3a05c6ff24fa830be88ce3053
Salim <salim.hassan.welleat@gmail.com>
2025-09-07 03:06:26 +0200

    added: home intro component

--- housori/src/app/features/home/home.module.ts
+++ housori/src/app/features/home/home.module.ts
@@ -4,9 +4,11 @@ import { HomeRoutingModule } from "./home-routing.module";
 import { FeaturedComponent } from "./components/featured/featured.component";
 import { CommonModule } from "@angular/common";
 import { FeaturedItemComponent } from "./components/featured-item/featured-item.component";
+import { IntroComponent } from "./components/intro/intro.component";
 
 @NgModule({
   declarations: [
+    IntroComponent,
     FeaturedItemComponent,
     HomeComponent,
     FeaturedComponent,
