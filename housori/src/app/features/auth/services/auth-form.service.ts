aa1405d980374954b0e5750a7984849e1494536a
Salim <doshka23@gmail.com>
2025-09-09 01:50:47 +0200

    refactored: auth form

--- housori/src/app/features/auth/services/auth-form.service.ts
+++ housori/src/app/features/auth/services/auth-form.service.ts
@@ -2,10 +2,27 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject, map, Observable } from "rxjs";
 import { AuthHttpService } from "./auth-http.service";
 
+export type AuthField = {
+  name: string;
+  label: string;
+  placeholder: string;
+  type:  string;
+  showIfLogin: boolean;
+ }
 @Injectable({providedIn: 'root'})
 export class AuthFormService {
   private submitFormSubject = new BehaviorSubject<'submit' | null>(null);
   private authFormValidationSubject = new BehaviorSubject<'VALID' | 'INVALID'>('INVALID');
+
+  // Define your form fields dynamically
+  formFields = [
+    { name: 'firstName', label: 'First name', placeholder: 'First name', type: 'text', showIfLogin: false },
+    { name: 'lastName', label: 'Last name', placeholder: 'Last name', type: 'text', showIfLogin: false },
+    { name: 'email', label: 'Your email', placeholder: 'Email', type: 'email', showIfLogin: true },
+    { name: 'password', label: 'Password', placeholder: 'Password', type: 'password', showIfLogin: true },
+    { name: 'passwordConfirm', label: 'Confirm password', placeholder: 'Confirm password', type: 'password', showIfLogin: false }
+  ];
+
   constructor(private authHttpService: AuthHttpService){}
 
 
