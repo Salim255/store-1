2e8d2ef0e2790563aee83f6d76bef84cbdcd8c60
Salim <salim.hassan.welleat@gmail.com>
2025-09-10 10:40:43 +0200

    fixed: routing bug

--- housori/src/app/navbar/navbar.module.ts
+++ housori/src/app/navbar/navbar.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from "@angular/core";
 import { NavbarRoutingModule } from "./navbar-routing.module";
 import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "./navbar.component";
-import { IonicModule } from "@ionic/angular";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { ToastrModule } from 'ngx-toastr';
+import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   imports:[
-    ToastrModule,
     NavbarRoutingModule,
     CommonModule,
-    IonicModule.forRoot(),
+    IonicModule
   ],
   declarations: [SidebarComponent, NavbarComponent ]
 })
